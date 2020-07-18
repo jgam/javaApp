f0b2ede27f9bc78070076f0ca8caab78922d9774
jgam <jgam@alumni.nd.edu>
2020-07-18 16:10:38 +0900

    [init] set done

---
+++ javaapp/src/components/SearchForm/FormContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import FormPresenter from './FormPresenter';
+
+function FormContainer() {
+    console.log('container')
+    return (
+        <FormPresenter></FormPresenter>
+    )
+}
+
+export default FormContainer
---
+++ javaapp/src/components/SearchForm/FormPresenter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FormPresenter() {
+    console.log('persenter')
+    return (
+        <div>
+            Heelo Form presenter!
+        </div>
+    )
+}
+
+export default FormPresenter;
